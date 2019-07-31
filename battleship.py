263ed55fcce7312b44d04a34e70d8ef55bee8388
nugentmichael <nugent.michael@gmail.com>
2019-07-31 09:16:09 -0400

    Increase player turns to 10, add hit counter variable, increase battleship size to 3, and resolve issues related to the increased grid size.

--- battleship.py
+++ battleship.py
@@ -20,48 +20,62 @@ ocean_board(grid)
 
 # Randomize the row order.
 def random_row(grid):
-  return randint(0, len(grid) - 1)
+  row = []
+  rand_row = randint(0, len(grid) - 1)
+  row += [rand_row - 1, rand_row, rand_row + 1]
+  return row
 
 # Randomize the column order.
 def random_col(grid):
-  return randint(0, len(grid[0]) - 1)
+  col = []
+  rand_col = randint(0, len(grid[0]) - 1)
+  col += [rand_col - 1, rand_col, rand_col + 1]
+  return col
 
 # Set the ship_row and ship_col variables to where the battleship is located on the grid.
 ship_row = random_row(grid)
 ship_col = random_col(grid)
 
 # Uncomment if you wish to display where the battleship is located on the grid.
-# print ship_row
-# print ship_col
+print ship_row
+print ship_col
+
+hit = 0
 
 # Run through player turns.
-for turn in range(4):
+for turn in range(9):
   # Display the current turn number and prompt the player for their guess to where the battleship is located on the grid.
   print "Turn", turn + 1
   guess_row = int(raw_input("Guess Row: "))
   guess_col = int(raw_input("Guess Column: "))
 
   # If the guessed row and column equals to where the battleship is located, the battleship has sunk and the game is now over.
-  if guess_row == ship_row and guess_col == ship_col:
-    print "You sunk my battleship!"
-    break
+  if guess_row in ship_row and guess_col in ship_col:
+    print "I've been hit!"
+    hit = hit + 1
+    print "Hit", hit
+    grid[guess_row][guess_col] = "X"
+
+    if hit == 3:
+      print "You sunk my battleship!"
+      break
   # If the battleship has not sunk, keep playing.
   else:
-	# If the guess is outside of the grid, display an error message.
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+    # If the guess is outside of the grid, display an error message.
+    if (guess_row < 0 or guess_row > 10) or (guess_col < 0 or guess_col > 10):
       print "The battleship is not docked. Please try another guess in the ocean."
-	# If the player has already guessed to where the battleship is location on the grid...
+    # If the player has already guessed to where the battleship is location on the grid...
     elif (grid[guess_row][guess_col] == "X"):
       print "You guessed that one already."
-	# If the player guessed an area to where the battleship is not located, they missed.
+    # If the player guessed an area to where the battleship is not located, they missed.
     else:
       print "You missed my battleship!"
       grid[guess_row][guess_col] = "X"
 
-	# Advance the turn number by one.
+    # Advance the turn number by one.
     print turn + 1
     ocean_board(grid)
-    
-	# After four turns, the game is now over.
-    if turn == 3:
+
+    # After four turns, the game is now over.
+    if turn == 9:
       print "Game Over!"
